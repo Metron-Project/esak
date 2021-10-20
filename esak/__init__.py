c1f63788621e1b6b9a53197fa93a1f193a1675eb
Brian Pepple <bdpepple@gmail.com>
2021-10-20 10:25:43 -0400

    Add some type hint & docstring to api()

--- esak/__init__.py
+++ esak/__init__.py
@@ -1,7 +1,24 @@
-from . import exceptions, session
+"""Project entry file."""
+__version__ = "1.0.0"
+
+from typing import Optional
+from esak import exceptions, session, sqlite_cache
 
 
-def api(public_key=None, private_key=None, cache=None):
+def api(
+    public_key: Optional[str] = None,
+    private_key: Optional[str] = None,
+    cache: Optional[sqlite_cache.SqliteCache] = None,
+) -> session.Session:
+    """Entry function the sets login credentials for Marvel's API.
+
+    :param public_key: The username used for metron.cloud.
+    :type public_key: str, optional
+    :param private_key: The password used for metron.cloud.
+    :type private_key: str, optional
+    :param cache: SqliteCache to use
+    :type cache: SqliteCache, optional
+    """
     if public_key is None:
         raise exceptions.AuthenticationError("Missing public_key.")
 
