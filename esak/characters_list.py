ee249349d57b7b64aedc6f2a128f8a6e2fb7144c
Brian Pepple <bdpepple@gmail.com>
2021-10-20 22:19:56 -0400

    Add some docstring for character & characters_list

--- esak/characters_list.py
+++ esak/characters_list.py
@@ -1,3 +1,10 @@
+"""
+CharactersList module.
+
+This module provides the following classes:
+
+- CharactersList
+"""
 import itertools
 
 from marshmallow import ValidationError
@@ -6,9 +13,11 @@ from . import character, exceptions
 
 
 class CharactersList:
+    """The CharactersList object contains a list of `Character` objects."""
+
     def __init__(self, response):
+        """Initialize a new CharactersList."""
         self.character = []
-        self.response = response
 
         schema = character.CharactersSchema()
         for character_dict in response["data"]["results"]:
@@ -20,12 +29,15 @@ class CharactersList:
             self.character.append(result)
 
     def __iter__(self):
+        """Return an iterator object."""
         return iter(self.character)
 
     def __len__(self):
+        """Return the length of the object."""
         return len(self.character)
 
     def __getitem__(self, index):
+        """Return the object of a at index."""
         try:
             return next(itertools.islice(self.character, index, index + 1))
         except TypeError:
