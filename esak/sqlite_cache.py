4b1aa15679ee1caad26c6f4f0d26c1f3f25a5c9b
Brian Pepple <bdpepple@gmail.com>
2021-10-20 22:27:27 -0400

    Add some docstrings for SqliteCache

--- esak/sqlite_cache.py
+++ esak/sqlite_cache.py
@@ -1,15 +1,34 @@
+"""
+SQLite Cache module.
+
+This module provides the following classes:
+
+- SqliteCache
+"""
 import json
 import sqlite3
 from typing import Any, Optional
 
 
 class SqliteCache:
+    """
+    The SqliteCache object to cache search results from Marvel.
+
+    :param str db_name: Path and database name to use.
+    """
+
     def __init__(self, db_name: str = "marvelous_cache.db") -> None:
+        """Intialize a new SqliteCache."""
         self.con = sqlite3.connect(db_name)
         self.cur = self.con.cursor()
         self.cur.execute("CREATE TABLE IF NOT EXISTS responses (key, json)")
 
     def get(self, key: str) -> Optional[Any]:
+        """
+        Retrieve data from the cache database.
+
+        :param str key: value to search for.
+        """
         self.cur.execute("SELECT json FROM responses WHERE key = ?", (key,))
         result = self.cur.fetchone()
 
@@ -19,6 +38,12 @@ class SqliteCache:
         return None
 
     def store(self, key: str, value: str) -> None:
+        """
+        Save data to the cache database.
+
+        :param str key: Item id.
+        :param str value: data to save.
+        """
         self.cur.execute(
             "INSERT INTO responses(key, json) VALUES(?, ?)", (key, json.dumps(value))
         )
