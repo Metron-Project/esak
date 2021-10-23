5f10ac6e4ccaba329a0346cf4e32b1c418137c4a
Brian Pepple <bdpepple@gmail.com>
2021-10-23 16:49:21 -0400

    Add docstrings for story_summary module

--- esak/story_summary.py
+++ esak/story_summary.py
@@ -10,7 +10,14 @@ from marshmallow import INCLUDE, Schema, fields, post_load, pre_load
 
 
 class StorySummary:
+    """
+    The StorySummary object contains basic information for Stories.
+
+    :param `**kwargs`: The keyword arguments used for setting story data from Marvel.
+    """
+
     def __init__(self, id=None, name=None, type=None, resource_uri=None, **kwargs):
+        """Intialize a new StorySummary."""
         self.id = id
         self.name = name
         self.type = type
@@ -19,19 +26,32 @@ class StorySummary:
 
 
 class StorySummarySchema(Schema):
+    """Schema for the StorySummary."""
+
     id = fields.Int()
     name = fields.Str()
     resourceURI = fields.Str(attribute="resource_uri")
     type = fields.Str()
 
     class Meta:
+        """Any unknown fields will be included."""
+
         unknown = INCLUDE
 
     @pre_load
     def process_input(self, data, **kwargs):
+        """Extract the Story Summary id."""
         data["id"] = data["resourceURI"].split("/")[-1]
         return data
 
     @post_load
     def make(self, data, **kwargs):
+        """
+        Make the StorySummary object.
+
+        :param data: Data from Marvel response.
+
+        :returns: :class:`StorySummary` object
+        :rtype: StorySummary
+        """
         return StorySummary(**data)
