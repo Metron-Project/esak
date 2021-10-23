9edda1bf417f87e972d7a53d8090ada62bf9d68b
Brian Pepple <bdpepple@gmail.com>
2021-10-23 16:53:57 -0400

    Add docstrings for creator_summary module

--- esak/creator_summary.py
+++ esak/creator_summary.py
@@ -10,7 +10,14 @@ from marshmallow import INCLUDE, Schema, fields, post_load, pre_load
 
 
 class CreatorSummary:
+    """
+    The CreatorSummary object contains basic information for creators.
+
+    :param `**kwargs`: The keyword arguments used for getting creator data from Marvel.
+    """
+
     def __init__(self, id=None, name=None, role=None, resource_uri=None, **kwargs):
+        """Intialize a new CreatorSummary."""
         self.id = id
         self.name = name
         self.role = role
@@ -19,20 +26,33 @@ class CreatorSummary:
 
 
 class CreatorSummarySchema(Schema):
+    """Schema for the CreatorSummary."""
+
     id = fields.Int()
     name = fields.Str()
     role = fields.Str()
     resourceURI = fields.Url(attribute="resource_uri")
 
     class Meta:
+        """Any unknown fields will be included."""
+
         unknown = INCLUDE
 
     @pre_load
     def process_input(self, data, **kwargs):
+        """Extract the Creator Summary id."""
         data["id"] = data["resourceURI"].split("/")[-1]
 
         return data
 
     @post_load
     def make(self, data, **kwargs):
+        """
+        Make the CreatorSummary object.
+
+        :param data: Data from Marvel response.
+
+        :returns: :class:`CreatorSummary` object
+        :rtype: CreatorSummary
+        """
         return CreatorSummary(**data)
