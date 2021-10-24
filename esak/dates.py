e73921e6550cbfd25a274fa22ddcba98a0b0880c
Brian Pepple <bdpepple@gmail.com>
2021-10-23 22:18:23 -0400

    Add docstrings for dates module

--- esak/dates.py
+++ esak/dates.py
@@ -1,8 +1,26 @@
+"""
+Dates module.
+
+This module provides the following classes:
+
+- Dates
+- DatesSchema
+"""
 from marshmallow import INCLUDE, Schema, fields, post_load, pre_load
 
 
 class Dates:
+    """
+    The Dates object contains common dates.
+
+    :param `on_sale`: The date the comic went on sale.
+    :param `foc`: The final order cutoff date.
+    :param `unlimited`: The date it was release on Marvel Unlimited.
+    :param `**kwargs`: The keyword arguments used for setting any other date from Marvel.
+    """
+
     def __init__(self, on_sale=None, foc=None, unlimited=None, **kwargs):
+        """Intialize a new date."""
         self.on_sale = on_sale
         self.foc = foc
         self.unlimited = unlimited
@@ -10,16 +28,21 @@ class Dates:
 
 
 class DatesSchema(Schema):
+    """Schema for the Dates."""
+
     onsaleDate = fields.Date(attribute="on_sale")
-    focDate = fields.Date(attribute="foc")  # Final Order Cutoff date
+    focDate = fields.Date(attribute="foc")
     unlimitedDate = fields.Date(attribute="unlimited")
 
     class Meta:
+        """Any unknown fields will be included."""
+
         unknown = INCLUDE
         dateformat = "%Y-%m-%dT%H:%M:%S%z"
 
     @pre_load
     def process_input(self, data, **kwargs):
+        """Clean up response data."""
         # Marvel comic 4373, and maybe others, returns a focDate of
         # "-0001-11-30T00:00:00-0500". The best way to handle this is
         # probably just to ignore it, since I don't know how to fix it.
@@ -27,4 +50,12 @@ class DatesSchema(Schema):
 
     @post_load
     def make(self, data, **kwargs):
+        """
+        Make the dates object.
+
+        :param data: Data from Marvel response.
+
+        :returns: :class:`Dates` object
+        :rtype: Dates
+        """
         return Dates(**data)
