8472a0761f57fa1e7e9a8c68f2ab12b38827d77e
Brian Pepple <bdpepple@gmail.com>
2021-11-14 12:30:56 -0500

    Add napolean config

--- docs/conf.py
+++ docs/conf.py
@@ -47,6 +47,8 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
 
 # -- Options for HTML output -------------------------------------------------
 
