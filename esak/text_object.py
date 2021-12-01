d238355367031b08a6474b578ac1993cc3ce320b
Brian Pepple <bdpepple@gmail.com>
2021-11-30 22:48:04 -0500

    Convert text_object docstring to numpy

--- esak/text_object.py
+++ esak/text_object.py
@@ -13,7 +13,19 @@ class TextObject:
     """
     The TextObject object contains basic information.
 
-    :param `**kwargs`: The keyword arguments used for getting data from Marvel.
+    Parameters
+    ----------
+    **kwargs
+        The keyword arguments used for setting textobject data from Marvel.
+
+    Attributes
+    ----------
+    type: str
+        The canonical type of the text object (e.g. solicit text, preview text, etc.).
+    language: str
+        The IETF language tag denoting the language the text object is written in.
+    text: str
+        The text.
     """
 
     def __init__(self, **kwargs):
@@ -34,9 +46,14 @@ class TextObjectSchema(Schema):
         """
         Make the TextObject object.
 
-        :param data: Data from Marvel response.
+        Parameters
+        ----------
+        data
+            Data from a Marvel API response.
 
-        :returns: :class:`TextObject` object
-        :rtype: TextObject
+        Returns
+        -------
+        TextObject
+            A TextObject object
         """
         return TextObject(**data)
